d1f0eb1afe4957ad4c4f7671cfef9cd6a3954632
Alexander <sashashvetsov3@gmail.com>
2022-03-03 19:45:27 +0300

    add section types - no adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,10 @@ const headerList = document.querySelector('.header__list')
 const toggleBtn = document.querySelector('.theme__switch')
 const theme = document.querySelector('.theme')
 
+const arrowLeft = document.querySelector('.types__slider-buttons-left')
+const arrowRight = document.querySelector('.types__slider-buttons-right')
+const itemSlider = document.querySelectorAll('.types__slider-item')
+let index = 0
 
 function toggleBurger() {
   headerList.classList.toggle('header__list_active')
@@ -19,8 +23,30 @@ function toggleButton(item) {
   item.classList.toggle('theme__switch_on')
 }
 
+function sliderRight() {
+  index++
+  if (index < itemSlider.length) {
+    itemSlider[index - 1].classList.remove('types__slider-item_active')
+    itemSlider[index].classList.add('types__slider-item_active')
+  } else {
+    itemSlider[index - 1].classList.remove('types__slider-item_active')
+    index = 0
+    itemSlider[index].classList.add('types__slider-item_active')
+  }
+}
 
-
+function sliderLeft() {
+  index--
+  console.log(index)
+  if (index >= 0) {
+    itemSlider[index + 1].classList.remove('types__slider-item_active')
+    itemSlider[index].classList.add('types__slider-item_active')
+  } else {
+    index = itemSlider.length - 1
+    itemSlider[index].classList.add('types__slider-item_active')
+    itemSlider[0].classList.remove('types__slider-item_active')
+  }
+}
 
 
 
@@ -28,6 +54,7 @@ toggleBtn.addEventListener('click', () => {
   toggleButton(toggleBtn)
 })
 headerBurgerOpenBtn.addEventListener('click', toggleBurger)
-
+arrowRight.addEventListener('click', sliderRight)
+arrowLeft.addEventListener('click', sliderLeft)
 
 
