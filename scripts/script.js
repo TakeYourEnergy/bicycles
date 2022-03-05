49f78c8e175fa7e71a8c15ec25720d9f42a24cec
Alexander <sashashvetsov3@gmail.com>
2022-03-05 20:30:10 +0300

    add adaptive tabs

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,74 @@ let index = 0
 const navigationItem = document.querySelectorAll('.tabs__navigation-item')
 const contentItem = document.querySelectorAll('.tabs__content-item')
 
+
+const select = document.querySelector('.tabs__select')
+const option = document.querySelectorAll('.tabs__option')
+select.addEventListener('change', () => {
+  const slHighway = document.querySelector('.tabs__slider-highway')
+  const slGravel = document.querySelector('.tabs__slider-grevel')
+  const slTt = document.querySelector('.tabs__slider-tt')
+
+  select.value === "highway" ? slHighway.classList.add('tabs__box-slider-item_active') : slHighway.classList.remove('tabs__box-slider-item_active')
+  select.value === "gravel" ? slGravel.classList.add('tabs__box-slider-item_active') : slGravel.classList.remove('tabs__box-slider-item_active')
+  select.value === "tt" ? slTt.classList.add('tabs__box-slider-item_active') : slTt.classList.remove('tabs__box-slider-item_active')
+})
+
+
+const linkPointGravel = document.querySelectorAll('.tabs__link-point-gravel')
+const tabsGravelSlider = document.querySelectorAll('.tabs__gravel-slider')
+linkPointGravel.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    let attrLinkFravel = e.target.getAttribute('data-gravel')
+    for (let i = 0; i < tabsGravelSlider.length; i++) {
+      let attrSlGravel = tabsGravelSlider[i].getAttribute('data-slgravel')
+      if (attrLinkFravel === attrSlGravel) {
+        tabsGravelSlider[i].classList.add('tabs__gravel-slider_active')
+      } else {
+        tabsGravelSlider[i].classList.remove('tabs__gravel-slider_active')
+      }
+    }
+  })
+})
+
+const linkPointTt = document.querySelectorAll('.tabs__link-point-tt')
+const tabsTtSlider = document.querySelectorAll('.tabs__tt-slider')
+console.log(tabsTtSlider);
+linkPointTt.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    let attrLinkTt = e.target.getAttribute('data-tt')
+    for (let i = 0; i < tabsTtSlider.length; i++) {
+      let attrSlTt = tabsTtSlider[i].getAttribute('data-sltt')
+      if (attrLinkTt === attrSlTt) {
+        tabsTtSlider[i].classList.add('tabs__tt-slider_active')
+      } else {
+        tabsTtSlider[i].classList.remove('tabs__tt-slider_active')
+      }
+    }
+  })
+})
+
+
+const linkPointHighway = document.querySelectorAll('.tabs__link-point-highway')
+const tabsBoxSlider = document.querySelectorAll('.tabs__highway-slider')
+linkPointHighway.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    let attrLinkHighWay = e.target.getAttribute('data-highway')
+    for (let i = 0; i < tabsBoxSlider.length; i++) {
+      let attrSlHighway = tabsBoxSlider[i].getAttribute('data-slhigway')
+      if (attrLinkHighWay === attrSlHighway) {
+        tabsBoxSlider[i].classList.add('tabs__highway-slider_active')
+      } else {
+        tabsBoxSlider[i].classList.remove('tabs__highway-slider_active')
+      }
+    }
+  })
+})
+
+
 function toggleBurger() {
   headerList.classList.toggle('header__list_active')
   if (headerList.classList.contains('header__list_active')) {
@@ -50,7 +118,6 @@ function sliderLeft() {
     itemSlider[0].classList.remove('types__slider-item_active')
   }
 }
-
 
 toggleBtn.addEventListener('click', () => {
   toggleButton(toggleBtn)
